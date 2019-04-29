8500ae8c4e7ec58abec45ac2c70a0e4d1335eff4
Luca Kiebel <github@luca-kiebel.de>
2019-04-30 00:21:50 +0200

    Update tuc-notendurchschnitt.js

--- tuc-notendurchschnitt.js
+++ tuc-notendurchschnitt.js
@@ -12,19 +12,19 @@
 
 (function() {
     'use strict';
-    Array.from(document.querySelectorAll("td")).find(t => t.innerHTML.toLowerCase() === "teilnehmer").parentNode.parentNode.innerHTML += `<tr>
-    		<td class="tabelleheader">Durchschnitt</td><td class="tabelle1" valign="top" id="durchschnitt333" align="right"></td>
-    	</tr>`
-    let tableInfo = Array.prototype.map.call(Array.from(document.querySelectorAll("td")).find(t => t.innerHTML.toLowerCase() === "teilnehmer").parentNode.parentNode.querySelectorAll("tr"), function(tr){
-  return Array.prototype.map.call(tr.querySelectorAll('td'), function(td){
-    return td.innerHTML;
-    });
-  });
-    tableInfo = tableInfo.slice(3,8).map(e => e[1])
-    let total = tableInfo.reduce((acc,cv) => {return acc+parseInt(cv)},0)
-    let i=1
-    let grades = tableInfo.reduce((acc,cv) => {return acc+i++*parseInt(cv)},0)
-    document.querySelector("#durchschnitt333").innerHTML = grades/total
-
-    // Your code here...
+    if(document.querySelectorAll("td")).find(t => t.innerHTML.toLowerCase() === "teilnehmer")) {
+        Array.from(document.querySelectorAll("td")).find(t => t.innerHTML.toLowerCase() === "teilnehmer").parentNode.parentNode.innerHTML += `<tr>
+                <td class="tabelleheader">Durchschnitt</td><td class="tabelle1" valign="top" id="durchschnitt333" align="right"></td>
+            </tr>`
+        let tableInfo = Array.prototype.map.call(Array.from(document.querySelectorAll("td")).find(t => t.innerHTML.toLowerCase() === "teilnehmer").parentNode.parentNode.querySelectorAll("tr"), function(tr){
+          return Array.prototype.map.call(tr.querySelectorAll('td'), function(td){
+            return td.innerHTML;
+            });
+          });
+        tableInfo = tableInfo.slice(3,8).map(e => e[1])
+        let total = tableInfo.reduce((acc,cv) => {return acc+parseInt(cv)},0)
+        let i=1
+        let grades = tableInfo.reduce((acc,cv) => {return acc+i++*parseInt(cv)},0)
+        document.querySelector("#durchschnitt333").innerHTML = grades/total
+    }
 })();
